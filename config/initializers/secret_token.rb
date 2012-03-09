9a25564efabac8d390ef29dfe837af01eb8586e8
Alex Le <alex@wordnik.com>
2012-03-09 13:15:56 -0800

    - add initial rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Heart::Application.config.secret_token = '9d55b61c6238c4589cf68fb20df3f323866ce928b58bff5fd7d22092ecb98e853ac321b9a09587ada266954562cf845c5f965f4dec3cb3a31fa68c673bf06f8d'
